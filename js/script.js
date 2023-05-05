e32c9e0217864e154cbfa7ca50399c5586b48437
Neveszera <gabrielgng26@gmail.com>
2023-05-05 09:56:14 -0300

    Array push/unshift/pop/shift/indexOf/splice

--- js/script.js
+++ js/script.js
@@ -33,10 +33,48 @@
 // 2ª Regra --> Não pode ser alterado durante o fluxo do programa.
 // nome3 = "Jonathan";
 
-let nome = "Beatriz";
+// let nome = "Beatriz";
 
-if (nome != "") {
-    let nome = "Robson" 
-}
+// if (nome != "") {
+//     let nome = "Robson" 
+// }
 
-console.log(nome);
+// console.log(nome);
+
+// ARRAY
+let frutas = ["Banana", "Maça", "Morango", "Carambola", "Acereola", "Laranja"];
+// IMPRIMIR
+console.log(frutas);
+console.table(frutas);
+console.log(frutas[4]);
+
+// Adicionar um item ao final do array com o método push();
+frutas.push("Melão");
+console.log(frutas);
+// Adicionar um item no início do array com o método unshift();
+frutas.unshift("Cajú");
+console.log(frutas);
+// Remover um item do final do array com o método unshift();
+frutas.push("Cajú");
+console.log(frutas);
+// Remover um item do final do array com o método pop();
+frutas.pop();
+console.log(frutas);
+// Remover um item no início do array com o método shift();
+frutas.shift();
+console.log(frutas);
+
+//Para localizar um itém dentro do array, utilizamos o método indexOf(itenNome);
+// let indice = frutas.indexOf("morango");
+// console.log(frutas[indice]);
+
+//Para localizar um itém dentro do array, e apagar ele apagar ele utilizamos o método splice(indice do item, quantidade de vezes que o indice será removido).
+//Obs: utilize o método indexOf(itenNome) para obter o indice do iten;
+let indice = frutas.indexOf("carambola");
+console.log("FRUTA Q ESTAVA NO INDICE ANTES DA REMOÇÃO : " + frutas[indice]);
+frutas.splice(indice,2);
+console.log("FRUTA Q FICOU NO INDICE PÓS REMOÇÃO : " + frutas[indice]);
+
+frutas.forEach( (fruta)=>{
+    console.log("Olha ele: " + fruta)
+} );
