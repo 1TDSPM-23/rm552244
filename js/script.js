cfb51ef25fec460d71609c52f2725b39b0937d95
Neveszera <gabrielgng26@gmail.com>
2023-05-05 09:23:44 -0300

    var/let/const dif- hoistig op/Mat. Interpolação.

---
+++ js/script.js
@@ -0,0 +1,42 @@
+// console.log("Olá Mundo!")
+
+// var nr1 = "10";
+// var nr2 = "5";
+
+// console.log(nr1);
+// console.log(nr2);
+
+// console.log("RESULTADO: " + (nr1 + nr2));
+// // console.log("RESULTADO SOMA: " + (parseInt(nr1) + parseInt(nr2)));
+// console.log(`RESULTADO SOMA : ${parseInt(nr1) + parseInt(nr2)}`);
+// console.log(`RESULTADO SUBTRAÇÃO : ${parseInt(nr1) - parseInt(nr2)}`);
+// console.log(`RESULTADO MULTIPLICAÇÃO : ${parseInt(nr1) * parseInt(nr2)}`);
+// console.log(`RESULTADO DIVISÃO : ${parseInt(nr1) / parseInt(nr2)}`);
+// console.log(`RESTO DIVISÃO : ${parseInt(nr1) % parseInt(nr2)}`);
+
+// -----------------------------------------------------------------------------------------------
+
+// var nr1 = "10";
+// var nr2 = "5";
+// var resutado;
+
+// resutado = !(parseInt(nr1)%parseInt(nr2)) ? "PAR" : "IMPAR";
+// console.log(`O NÚMERO É : ${resutado}`);
+
+// DIFERENÇA VAR/LET/CONST
+// var nome1 = "Beatriz";
+// let nome2 = "Matheus";
+
+// REGRAS
+// 1ª Regra --> Inicializar na declaração
+// const nome3 = "Violante";
+// 2ª Regra --> Não pode ser alterado durante o fluxo do programa.
+// nome3 = "Jonathan";
+
+let nome = "Beatriz";
+
+if (nome != "") {
+    let nome = "Robson" 
+}
+
+console.log(nome);
