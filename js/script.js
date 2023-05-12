fa530a561aa8fe2b94a4062afd665b911fe46ac7
Neveszera <gabrielgng26@gmail.com>
2023-05-12 08:21:22 -0300

    Forms - Exemplos / Manipulação

--- js/script.js
+++ js/script.js
@@ -42,27 +42,27 @@
 // console.log(nome);
 
 // ARRAY
-let frutas = ["Banana", "Maça", "Morango", "Carambola", "Acereola", "Laranja"];
-// IMPRIMIR
-console.log(frutas);
-console.table(frutas);
-console.log(frutas[4]);
+// let frutas = ["Banana", "Maça", "Morango", "Carambola", "Acereola", "Laranja"];
+// // IMPRIMIR
+// console.log(frutas);
+// console.table(frutas);
+// console.log(frutas[4]);
 
-// Adicionar um item ao final do array com o método push();
-frutas.push("Melão");
-console.log(frutas);
-// Adicionar um item no início do array com o método unshift();
-frutas.unshift("Cajú");
-console.log(frutas);
-// Remover um item do final do array com o método unshift();
-frutas.push("Cajú");
-console.log(frutas);
-// Remover um item do final do array com o método pop();
-frutas.pop();
-console.log(frutas);
-// Remover um item no início do array com o método shift();
-frutas.shift();
-console.log(frutas);
+// // Adicionar um item ao final do array com o método push();
+// frutas.push("Melão");
+// console.log(frutas);
+// // Adicionar um item no início do array com o método unshift();
+// frutas.unshift("Cajú");
+// console.log(frutas);
+// // Remover um item do final do array com o método unshift();
+// frutas.push("Cajú");
+// console.log(frutas);
+// // Remover um item do final do array com o método pop();
+// frutas.pop();
+// console.log(frutas);
+// // Remover um item no início do array com o método shift();
+// frutas.shift();
+// console.log(frutas);
 
 //Para localizar um itém dentro do array, utilizamos o método indexOf(itenNome);
 // let indice = frutas.indexOf("morango");
@@ -70,11 +70,14 @@ console.log(frutas);
 
 //Para localizar um itém dentro do array, e apagar ele apagar ele utilizamos o método splice(indice do item, quantidade de vezes que o indice será removido).
 //Obs: utilize o método indexOf(itenNome) para obter o indice do iten;
-let indice = frutas.indexOf("carambola");
-console.log("FRUTA Q ESTAVA NO INDICE ANTES DA REMOÇÃO : " + frutas[indice]);
-frutas.splice(indice,2);
-console.log("FRUTA Q FICOU NO INDICE PÓS REMOÇÃO : " + frutas[indice]);
+// let indice = frutas.indexOf("carambola");
+// console.log("FRUTA Q ESTAVA NO INDICE ANTES DA REMOÇÃO : " + frutas[indice]);
+// frutas.splice(indice,2);
+// console.log("FRUTA Q FICOU NO INDICE PÓS REMOÇÃO : " + frutas[indice]);
 
-frutas.forEach( (fruta)=>{
-    console.log("Olha ele: " + fruta)
-} );
+// frutas.forEach( (fruta)=>{
+//     console.log("Olha ele: " + fruta)
+// } );
+
+const inputUser = document.getElementById("idNm");
+ console.log(inputUser.value);
